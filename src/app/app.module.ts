f85678aa515faae6068babe738e910c852355e31
Daniel <danielribas1105@gmail.com>
2023-05-30 09:50:37 -0300

    create new angular study project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputComponent } from './components/input/input.component';
+import { ItemComponent } from './components/item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
